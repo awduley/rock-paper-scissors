e45118a73320af31634ef2d672bfc4bc0ec1e291
awduley <duleyalaska@gmail.com>
2025-04-12 14:28:49 -0700

    Add ui to the rock, paper, scissors game
    User can now play this game by clicking on a
    button to make a selection rather than typing in their
    selection

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+
+const human = document.querySelector('.human');
+const computer = document.querySelector('.computer');
+const winner = document.querySelector('.winner');
+
 function getComputerChoice() {
   let randNum = Math.floor(Math.random () * 3) + 1;
   if(randNum === 1) {
@@ -9,48 +17,72 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  let ourChoice = prompt("Do you choose rock, paper, or scissors?: ")
-  return ourChoice.toLowerCase();
+function welcomeMessage() {
+  alert("Welcome to Rock, Paper, Scissors. Begin by selecting your option below. First to five points wins!");
 }
 
 function playGame() {
+
+  welcomeMessage();
 
   // Variables to keep track of the scores
   let computerScore = 0;
   let humanScore = 0;
 
+  human.textContent = humanScore;
+  computer.textContent = computerScore;
+
+  rock.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    playRound(computerSelection, "rock")
+  })
+
+  paper.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    playRound(computerSelection, "paper")
+  })
+
+  scissors.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    playRound(computerSelection, "scissors")
+  })
+
   // Function to play one round of rock, paper, scissors
   function playRound(computerChoice, humanChoice) {
 
-  if (computerChoice === "Rock" && humanChoice === "scissors") {
-    console.log("You lose! " + computerChoice + " beats " + humanChoice);
-    computerScore ++;
-  } else if (computerChoice === "Paper" && humanChoice === "rock") {
-    console.log("You lose! " + computerChoice + " beats " + humanChoice);
-    computerScore ++;
-  } else if (computerChoice === "Scissors" && humanChoice === "paper") {
-    console.log("You lose! " + computerChoice + " beats " + humanChoice)
-    computerScore ++;
-  } else if (computerChoice.toLowerCase() === humanChoice) {
-    console.log("It's a tie! You both chose " + computerChoice)
-  } else {
-    console.log("You win! " + humanChoice + " beats " + computerChoice);
-    humanScore ++;
-  }
-  }
+    if (computerChoice === "Rock" && humanChoice === "scissors") {
+      console.log("You lose! " + computerChoice + " beats " + humanChoice);
+      computerScore ++;
+      computer.textContent = computerScore;
+    } else if (computerChoice === "Paper" && humanChoice === "rock") {
+      console.log("You lose! " + computerChoice + " beats " + humanChoice);
+      computerScore ++;
+      computer.textContent = computerScore;
+    } else if (computerChoice === "Scissors" && humanChoice === "paper") {
+      console.log("You lose! " + computerChoice + " beats " + humanChoice)
+      computerScore ++;
+      computer.textContent = computerScore;
+    } else if (computerChoice.toLowerCase() === humanChoice) {
+      console.log("It's a tie! You both chose " + computerChoice)
+    } else {
+      console.log("You win! " + humanChoice + " beats " + computerChoice);
+      humanScore ++;
+      human.textContent = humanScore;
+    }
 
-  // For loop to call the playRound function five times
-  for(let i = 1; i <= 5; i ++) {
-    // Variables to get random selections each time the playRound function is called
-    const computerSelection = getComputerChoice();
-    const humanSelection = getHumanChoice();
-    playRound(computerSelection, humanSelection);
-  }
-  if (computerScore > humanScore) {
-    console.log(`Sorry, you loose! The computer finished with ${computerScore}, while you only had ${humanScore}.`)
-  } else {
-    console.log(`Congratulations, you won! You finished with ${humanScore}, while the computer only had ${computerScore}.`)
+    // If statements to end the game when one player reaches five points
+    if (computerScore === 5) {
+
+      console.log(`Sorry, you loose! The computer finished with ${computerScore}, while you only had ${humanScore}.`);
+      computer.classList.add('red');
+      winner.textContent = 'Computer Wins...'
+    } 
+    
+    if (humanScore === 5) {
+      console.log(`Congratulations, you won! You finished with ${humanScore}, while the computer only had ${computerScore}.`)
+      human.classList.add('green');
+      winner.textContent = 'You Win!'
+    } 
   }
 }
 
